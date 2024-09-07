995cd294855824a998f3f02f603ea8772948d4a6
Muha11ad <abdushukurovmukhammadjon99@gmail.com>
2024-09-07 13:12:16 +0500

    npm run build fixed dist folder is in dist repository

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths'; // Import the plugin
 
 export default defineConfig({
     plugins: [
-        react(),
-        svgr({
-            include: '**/*.svg?react',
-        }),
+      react(),
+      svgr(),
+      tsconfigPaths() // Added here
     ],
     base: '/IT-Bookstore/',
     resolve: {
-        alias: {
-            app: '/src/app',
-            entities: '/src/entities',
-            features: '/src/features',
-            pages: '/src/pages',
-            shared: '/src/shared',
-            widgets: '/src/widgets',
-        },
+      alias: {
+        app: '/src/app',
+        entities: '/src/entities',
+        features: '/src/features',
+        pages: '/src/pages',
+        shared: '/src/shared',
+        widgets: '/src/widgets',
+      },
     },
     server: {
-        port: 3000,
+      port: 3000,
     },
-})
+  });
+  
